b83914b4f6f6069cefa1f171124459d76cb2f216
Maria Eduarda <mariaeduardastutz260608@gmail.com>
2024-11-05 17:17:21 -0200

    feature: Adicionando tabela

---
+++ aula01/aula1.php
@@ -0,0 +1,23 @@
+<?php
+$dadosFuncionarios = array(
+    array('nome'=> 'João', 'idade'=>23, 'cidade'=>'nova friburgo'),
+    array('nome'=> 'José', 'idade'=>25, 'cidade'=>'nova friburgo'),
+    array('nome'=> 'Maria', 'idade'=>22, 'cidade'=>'nova friburgo')
+);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$montaTabela = function($dados){
+    $retorno = "<table><tr><tr><th>Nome<th/><th>idade</th><th>Cidade</th></tr>";
+}
+?>
+</body>
+</html>
